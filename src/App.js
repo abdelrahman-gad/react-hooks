829efe82b636148a795a89de35ca93e9bc946a13
abdelrahman-gad <abdelrahmangad95@gmail.com>
2021-02-25 22:15:12 +0200

    adding uesRef hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {useState , useEffect , useRef} from 'react';
 function App() {
-  return (
+  const [name , setName ]  = useState('');
+  const renderCount  = useRef(0);
+
+  //  used to store the proevious value of a state
+  const prevName = useRef('');
+  useEffect(() => {
+     prevName.current = name;
+  }, [name]);
+
+
+  // useEffect(() => {
+  //   renderCount.current = renderCount.current +1;   
+  // });
+  const inputRef = useRef();
+  // when you click the button it will call the 'focus' method which will  put the value in but without rerendering the state 
+  const focus = () => {
+    inputRef.current.focus();
+    inputRef.current.value='Some value';
+
+  }
+  return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <input ref={inputRef} value={name} onChange={ (e) => setName(e.target.value)  } ></input>
+      <h3>  My name  is <h2> { name } </h2> and it used to be <h2> {prevName.current}  </h2>  </h3>
+      <h4> I rendered   {renderCount.current}  times </h4>
+      <button onClick={focus}>Focus </button>
     </div>
   );
 }
